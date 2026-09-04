dd0e13c75f8538c95ddcbb652c9a83be9039b97d
ubtox <dave1pro@outlook.com>
2026-09-04 14:55:29 +0200

    LAB: add UIA text hardening regression tests

--- NVDA-2026.2-HARDENED-SOURCE-V5/tests/unit/test_UIAHandler.py
+++ NVDA-2026.2-HARDENED-SOURCE-V5/tests/unit/test_UIAHandler.py
@@ -1,13 +1,17 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2026 NV Access Limited, Leonard de Ruijter
+# Copyright (C) 2026 NV Access Limited, Leonard de Ruijter, Tobias Heath
 # This file may be used under the terms of the GNU General Public License, version 2 or later, as modified by the NVDA license.
 # For full terms and any additional permissions, see the NVDA license file:
 # https://github.com/nvaccess/nvda/blob/master/copying.txt
 
 from unittest import TestCase
+from unittest.mock import Mock
+
+from comtypes import COMError
 
 import textInfos
-from UIAHandler import NVDAUnitsToUIAUnits, getUIAUnitFromNVDAUnit
+import UIAHandler
+from UIAHandler import NVDAUnitsToUIAUnits, getUIAUnitFromNVDAUnit, utils
 
 
 class Test_getUIAUnitFromNVDAUnit(TestCase):
@@ -20,3 +24,76 @@ class Test_getUIAUnitFromNVDAUnit(TestCase):
 	def test_unmappedUnitRaisesNotImplementedError(self):
 		with self.assertRaises(NotImplementedError):
 			getUIAUnitFromNVDAUnit(textInfos.UNIT_SENTENCE)
+
+
+class TestNormalizeUIAText(TestCase):
+	def test_plainTextReturnedUnchanged(self):
+		text = "NVDA WinUI 3"
+		self.assertIs(utils.normalizeUIAText(text), text)
+
+	def test_validSurrogatePairBecomesUnicodeScalar(self):
+		self.assertEqual(utils.normalizeUIAText("\ud83d\ude00"), "😀")
+
+	def test_isolatedHighSurrogateIsReplaced(self):
+		self.assertEqual(utils.normalizeUIAText("A\ud83dB"), "A\ufffdB")
+
+	def test_isolatedLowSurrogateIsReplaced(self):
+		self.assertEqual(utils.normalizeUIAText("A\ude00B"), "A\ufffdB")
+
+
+class TestUIATextRangeFromElement(TestCase):
+	def test_standardRangeFromChildHasPriority(self):
+		textPattern = Mock()
+		standardRange = Mock()
+		textPattern.rangeFromChild.return_value = standardRange
+		element = Mock()
+
+		self.assertIs(utils.UIATextRangeFromElement(textPattern, element), standardRange)
+		element.GetCurrentPattern.assert_not_called()
+
+	def test_textChildPatternFallbackAfterCOMError(self):
+		textPattern = Mock()
+		textPattern.rangeFromChild.side_effect = COMError(-1, "failure", None)
+		documentRange = Mock()
+		textPattern.documentRange = documentRange
+		element = Mock()
+		textChildRange = Mock()
+		textChildPattern = Mock(TextRange=textChildRange)
+		punk = Mock()
+		punk.QueryInterface.return_value = textChildPattern
+		element.GetCurrentPattern.return_value = punk
+
+		self.assertIs(utils.UIATextRangeFromElement(textPattern, element), textChildRange)
+		element.GetCurrentPattern.assert_called_once_with(UIAHandler.UIA_TextChildPatternId)
+		textChildRange.CompareEndpoints.assert_called_once_with(
+			UIAHandler.TextPatternRangeEndpoint_Start,
+			documentRange,
+			UIAHandler.TextPatternRangeEndpoint_Start,
+		)
+
+	def test_textChildPatternFallbackAfterNullRange(self):
+		textPattern = Mock()
+		textPattern.rangeFromChild.return_value = None
+		textPattern.documentRange = Mock()
+		element = Mock()
+		textChildRange = Mock()
+		textChildPattern = Mock(TextRange=textChildRange)
+		punk = Mock()
+		punk.QueryInterface.return_value = textChildPattern
+		element.GetCurrentPattern.return_value = punk
+
+		self.assertIs(utils.UIATextRangeFromElement(textPattern, element), textChildRange)
+
+	def test_textChildPatternFallbackRejectsRangeFromDifferentTextProvider(self):
+		textPattern = Mock()
+		textPattern.rangeFromChild.return_value = None
+		textPattern.documentRange = Mock()
+		element = Mock()
+		textChildRange = Mock()
+		textChildRange.CompareEndpoints.side_effect = COMError(-1, "different provider", None)
+		textChildPattern = Mock(TextRange=textChildRange)
+		punk = Mock()
+		punk.QueryInterface.return_value = textChildPattern
+		element.GetCurrentPattern.return_value = punk
+
+		self.assertIsNone(utils.UIATextRangeFromElement(textPattern, element))
